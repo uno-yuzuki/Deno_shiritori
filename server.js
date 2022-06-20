643151cbce99726e88e554e7a95e55b353396a83
宇野夢津樹 <56952247+uno-yuzuki@users.noreply.github.com>
2022-06-20 18:34:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.138.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
 
 let previousWord = "しりとり";
-let n="ん";
+let notWord = "ん";
 
 console.log("Listening on http://localhost:8000");
 serve(async (req) => {
@@ -19,7 +19,7 @@ serve(async (req) => {
         nextWord.length > 0 &&previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)) {
         return new Response("前の単語に続いていません。", { status: 400 });
       }
-    if (nextWord.charAt(next.length - 1) === n.charAt(0)) {
+    if (nextWord.charAt(next.length - 1) === notWord.charAt(0)) {
       return new Response("「ん」が付いています。", { status: 400 });
     }
 
