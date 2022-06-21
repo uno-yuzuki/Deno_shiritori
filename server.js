8f00442d24f9338f2c4878a5a567d2e1cb607de7
宇野夢津樹 <56952247+uno-yuzuki@users.noreply.github.com>
2022-06-21 16:10:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -36,3 +36,19 @@ serve(async (req) => {
     enableCors: true,
   });
 });
+
+$(function () {
+  var audioBtn = $('.audio_button'),
+  audioWrap = $('.audio_wrap'),
+  audio = document.getElementById('audio');
+
+  audioBtn.on('click', function () {
+    if( audioWrap.hasClass('play') ) {
+      audio.pause();
+      audioWrap.removeClass('play');
+    } else {
+      audio.play();
+      audioWrap.addClass('play');
+    }
+  });
+});
